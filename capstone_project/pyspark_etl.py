7d3355015c897783993521e584edec72734ae551
dsalaj <salaj.au@gmail.com>
2020-07-07 15:58:30 +0200

    Add spark parsing of webhose news

--- capstone_project/pyspark_etl.py
+++ capstone_project/pyspark_etl.py
@@ -12,12 +12,11 @@ LOCAL = True
 def create_spark_session():
     spark = SparkSession \
         .builder \
-        .config("spark.driver.memory", "15g") \
+        .config("spark.driver.memory", "16g") \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
     return spark
 
-#news_data = os.path.join(data_path, 'News_filtered.json')
 
 def process_reddit_submissions(spark, data_path):
     reddit_data = os.path.join(data_path, 'RS_filtered.json')
@@ -51,14 +50,46 @@ def process_reddit_submissions(spark, data_path):
         .format("parquet") \
         .save(out_path)
 
+    
+def process_news_articles(spark, data_path):
+    news_data = os.path.join(data_path, 'News_filtered.json')
+
+    schema = StructType([
+        StructField("likes", IntegerType(), True),
+        StructField("text", StringType(), True),
+        StructField("published", StringType(), True),  # 2017-02-03T08:42:00.000+02:00
+    ])
+    
+    # read data file
+    df = spark.read.schema(schema).json(news_data)
+    
+    # yyyy-MM-dd'T'HH:mm:ss.SSS'Z'
+    df = df.withColumn('date', to_timestamp("published").cast(DateType()))
+
+    # extract columns to create a table
+    table = df.select(
+        "text",
+        "likes",
+        "date",
+        dayofmonth("date").alias("day"),
+        month("date").alias("month"),
+        year("date").alias("year"),
+    )
+    table.show(n=10)
+    
+    # write table to parquet files partitioned by month and day
+    out_path = os.path.join(data_path, 'news.parquet')
+    table.write \
+        .partitionBy("month", "day") \
+        .format("parquet") \
+        .save(out_path)
+
 
 def main():
     spark = create_spark_session()
     data_path = "./" if LOCAL else config.get('default', 'S3')
-    process_reddit_submissions(spark, data_path)
-    # df.limit(2).toPandas()
-#     process_song_data(spark, input_data, output_data)
-#     process_log_data(spark, input_data, output_data)
+    # process_reddit_submissions(spark, data_path)
+    process_news_articles(spark, data_path)
 
 
 if __name__ == "__main__":
